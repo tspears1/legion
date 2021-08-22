ece0cad76a2540138fb408e0e8a34c9d838badab
Tim Spears <timspears@imarc.com>
2021-08-22 16:09:39 -0400

    setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,17 @@ module.exports = {
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://legion.timothyspears.com/graphql`,
+        debug: {
+          graphql: {
+            showQueryOnError: true,
+          },
+        },
+      }
+    }
   ],
 }
