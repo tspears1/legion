dba5709061eea388cd7240999f1aa1616fca31a4
Tim Spears <timspears@imarc.com>
2021-08-22 16:14:41 -0400

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great. Or Else.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
